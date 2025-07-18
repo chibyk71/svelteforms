702fdee667bb70d6dcf7021b3e1ee6412ae8706b
ALFRED CHIBUIKE THEOPHANE <chibyk089@gmail.com>
2025-07-18 11:38:59 +0100

    Refactor Input Component: Bind Value and Improve Props Handling
    - Added two-way data binding for the input value using `bind:value`.
    - Enhanced props handling to ensure the `name` attribute is correctly applied to the input element.
    - Improved class management with the `cn` utility for better styling consistency.
    - Maintained existing functionality while enhancing the overall structure and readability of the component.

--- src/lib/components/fields/Input.svelte
+++ src/lib/components/fields/Input.svelte
@@ -2,10 +2,10 @@
 	import { cn } from "$lib/index.js";
 	import type { Attributes } from "$lib/types.js";
 
-    let { type = 'text', name, value = '', class:className, ...restProps }: {
+    let { type = 'text', name, value = $bindable(''), class:className, ...restProps }: {
         name?: string;
         value?: string;
     } & Attributes<'input'> = $props();
 </script>
 
-<input class={cn("ease peer w-full rounded-md border border-slate-200 bg-transparent py-2 pr-3 pl-3 text-sm text-slate-700 shadow-sm transition duration-300 placeholder:text-slate-400 hover:border-slate-300 focus:border-slate-400 focus:shadow focus:outline-none", className)} {...restProps}/>
+<input {type} class={cn("ease peer w-full rounded-md border border-slate-200 bg-transparent py-2 pr-3 pl-3 text-sm text-slate-700 shadow-sm transition duration-300 placeholder:text-slate-400 hover:border-slate-300 focus:border-slate-400 focus:shadow focus:outline-none", className)} bind:value={value} {name} {...restProps}/>
