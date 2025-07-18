7911d65ea7828d992798cc3cb3db968236030ba7
ALFRED CHIBUIKE THEOPHANE <chibyk089@gmail.com>
2025-07-18 11:36:51 +0100

    Refactor SmartField Component: Improve Structure and Styling
    - Enhanced the layout structure for better readability and maintainability.
    - Added conditional rendering for checkbox and radio input types without options.
    - Improved styling for the input container, including dark mode support.
    - Streamlined the rendering of prefix and suffix elements.
    - Ensured error handling for dynamic component loading is clear and user-friendly.
    - Maintained existing functionality while improving overall code organization.

--- src/lib/components/SmartField.svelte
+++ src/lib/components/SmartField.svelte
@@ -28,34 +28,41 @@
 	const InputComponent = getComponent(type);
 </script>
 
-<div class="relative w-full max-w-sm min-w-[200px] [not:first-child]:mt-4">
-	<Label {id} {label} {float} />
-
-	<div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
-		{#if loading}
-			<span class="shrink-0 text-slate-400">
-				<Spinner />
-			</span>
-		{:else if suffix}
-			<span class="shrink-0 text-slate-400">
-				{@render suffix()}
-			</span>
-		{/if}
-
+<div class="relative w-full min-w-[200px] mt-4">
+	{#if (type === 'checkbox' || type === 'radio') && !options}
 		{#await InputComponent then Component}
-			<Component.default {name} {...restProps} bind:value {...attributes} {options} />
+			<Component.default {name} {...restProps} bind:value {...attributes} options={[{label, value:true}]} />
 		{:catch error}
 			<p class="text-red-500">Error loading component: {error.message}</p>
 		{/await}
-
-		{#if prefix}
-			<span class="shrink-0 text-slate-400">
-				{@render prefix()}
-			</span>
-		{/if}
-
-		{#if description}
-			<Message message={description} {isInvalid} />
-		{/if}
-	</div>
+	{:else}
+		<Label {id} {label} {float} />
+	
+		<div class="flex items-center rounded-md bg-white dark:bg-slate-600 dark:text-slate-100 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
+			{#if loading}
+				<span class="shrink-0 text-slate-400">
+					<Spinner />
+				</span>
+			{:else if suffix}
+				<span class="shrink-0 text-slate-400">
+					{@render suffix()}
+				</span>
+			{/if}
+	
+			{#await InputComponent then Component}
+				<Component.default {name} {...restProps} bind:value {...attributes} {options} />
+			{:catch error}
+				<p class="text-red-500">Error loading component: {error.message}</p>
+			{/await}
+	
+			{#if prefix}
+				<span class="shrink-0 text-slate-400">
+					{@render prefix()}
+				</span>
+			{/if}
+		</div>
+	{/if}
+	{#if description}
+		<Message message={description} {isInvalid} />
+	{/if}
 </div>
