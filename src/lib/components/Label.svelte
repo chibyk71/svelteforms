d69ccd3b8d9bcf058aa8d954d6b8a19776c60a10
ALFRED CHIBUIKE THEOPHANE <chibyk089@gmail.com>
2025-07-18 11:34:57 +0100

    Enhance Label Component: Add Class Prop and Improve Styling
    - Introduced a `class` prop to allow custom styling for the label.
    - Refactored label classes to use a base class and utility function for better maintainability.
    - Improved floating label styles for better visual consistency.

--- src/lib/components/Label.svelte
+++ src/lib/components/Label.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
-    let { id, label, float }: { id:string, label:string, float: boolean } = $props();
+	import { base } from "$app/paths";
+	import { cn } from "$lib/index.js";
+
+    let { id, label, float, class:classes }: { id:string, label:string, float: boolean, class?: string } = $props();
+
+	let baseClass = 'mb-2 block text-base/tight text-slate-600 dark:text-slate-200';
 </script>
 
 {#if float}
-	<label
-		for={id}
-		class="absolute top-2.5 left-2.5 origin-left transform cursor-text bg-white px-1 text-sm text-slate-400 transition-all peer-focus:-top-2 peer-focus:left-2.5 peer-focus:scale-90 peer-focus:text-xs peer-focus:text-slate-400"
-	>
+	<label for={id}
+		class={cn(base, "absolute top-2.5 left-2.5 origin-left transform cursor-text bg-white px-1 transition-all peer-focus:-top-2 peer-focus:left-2.5 peer-focus:scale-90 peer-focus:text-xs peer-focus:text-slate-400", classes)}>
 		{label}
 	</label>
 {:else}
-	<label for={id} class="mb-2 block text-sm text-slate-600"> {label} </label>
+	<label for={id} class={cn(baseClass, classes)}> {label} </label>
 {/if}
